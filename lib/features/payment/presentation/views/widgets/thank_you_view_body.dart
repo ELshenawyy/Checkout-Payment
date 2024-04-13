9bd787f30abff84e74e18cba435de715735c4350
ELshenawyy <alshnawyahmd668@gmail.com>
2024-04-13 08:51:45 +0200

    Custom Shaped Container

--- lib/features/payment/presentation/views/widgets/thank_you_view_body.dart
+++ lib/features/payment/presentation/views/widgets/thank_you_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ThankYouViewBody extends StatelessWidget {
@@ -5,8 +6,33 @@ class ThankYouViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                color: const Color(0xffD9D9D9)),
+          ),
+          Positioned(
+            bottom: MediaQuery.sizeOf(context).height * 0.2,
+            left: -20,
+            child: const CircleAvatar(
+              backgroundColor: Colors.white,
+            ),
+          ),
+          Positioned(
+            bottom: MediaQuery.sizeOf(context).height * 0.2,
+            right: -20,
+            child: const CircleAvatar(
+              backgroundColor: Colors.white,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
