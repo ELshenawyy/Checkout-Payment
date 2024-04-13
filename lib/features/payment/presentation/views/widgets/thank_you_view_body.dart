868c0d0e2cdd6f9e1edd9e8df4cdb6063f95e62d
ELshenawyy <alshnawyahmd668@gmail.com>
2024-04-13 13:43:03 +0200

    Create Dashed Line

--- lib/features/payment/presentation/views/widgets/thank_you_view_body.dart
+++ lib/features/payment/presentation/views/widgets/thank_you_view_body.dart
@@ -49,6 +49,25 @@ class ThankYouViewBody extends StatelessWidget {
               ),
             ),
           ),
+          Positioned(
+            left: 20 + 8,
+            right: 20 + 8,
+            bottom: MediaQuery.sizeOf(context).height * 0.2 + 20,
+            child: Row(
+              children: List.generate(
+                30,
+                (index) => Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2),
+                    child: Container(
+                      color: const Color(0xffB8B8B8),
+                      height: 2,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
