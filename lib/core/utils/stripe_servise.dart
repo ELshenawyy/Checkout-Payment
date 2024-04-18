ab407cbbe53eeeed7a336fa9699f16ecc4a693ab
ELshenawyy <alshnawyahmd668@gmail.com>
2024-04-18 10:46:28 +0200

    create Stripe servise & api servise

---
+++ lib/core/utils/stripe_servise.dart
@@ -0,0 +1,9 @@
+import 'package:checkout_payment1/features/payment/payment_intent_input_model.dart';
+
+class StripeServise 
+{
+ //PaymentIntentInputModel createPaymentIntent(PaymentIntentInputModel PaymentIntentInputModel ) 
+ //{
+  //return 
+ //}
+}
